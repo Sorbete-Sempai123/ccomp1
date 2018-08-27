b13d72c3e3243c66ce3ea3476f0f79b8f7e8f1f5
Sorbete-Sempai123 <tipon_tn@hotmail.com>
2018-08-27 13:48:06 -0500

    ward

--- helloworld/main.cpp
+++ helloworld/main.cpp
@@ -2,11 +2,45 @@
 
 using namespace std;
 
+int factorial(int n){
+    int acum=1;
+    while(n>1){acum=acum*n,n--;}
+    return acum;
+}
+
+int primo(int n){
+    if (n%2==0)return 0;
+    int acum=0,counter=1;
+    while(counter<(n/2)){
+    if (n%counter==0)acum++;
+    if(acum>2)return 0;
+    counter=counter+2;
+    }
+    return 1;
+}
+
+int comparar(int n,int m){
+    if(n<m)cout<<m<<" Es mayor que "<<n<<endl;
+    else if (n==m)cout<<n<<" Es igual que "<<m<<endl;
+    else printf("%d Es mayor que %d ",n,m);
+}
+
+int comparar3(int n,int m,int o){
+    if(n>m && n>o){if(m>o)cout<<n<<" Es mayor que "<<m<<" Es mayor que "<<o;else cout<<n<<" Es mayor que "<<o<<" Es mayor que "<<m;}
+    else if(m>n && m>o){if(n>o)cout<<m<<" Es mayor que "<<n<<" Es mayor que "<<o;else cout<<m<<" Es mayor que "<<o<<" Es mayor que "<<n;}
+    else if (m==n && n==o) cout<<"Todos son iguales";
+    else {if(n>m)cout<<o<<" Es mayor que "<<n<<" Es mayor que "<<m;else cout<<o<<" Es mayor que "<<m<<" Es mayor que "<<n;}
+}
+
 int main(){
-     int a=2147483647;
-    int b=a;
-    cout <<b<< endl;
-    b=b+1;
-    cout <<b<< endl;
-    return 0;
+    int n;
+    cin>>n;
+    //cout<<factorial(n)<<endl;
+    cout<<primo(n)<<endl;
+    int a,b,c;
+
+    cout<<"Escriba los 3 numeros\n";cin>>a>>b>>c;
+    comparar3(a,b,c);
+
+
 }
